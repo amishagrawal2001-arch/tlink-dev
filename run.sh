76558d0d4160f150d8f0c56e5e6afd23cb5a1ea0
amishagrawal2001-arch <amishagrawal2001@gmail.com>
2026-01-30 23:00:10 -0800

    Update run script for debug startup

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-TLINK_DEV=1 ./node_modules/.bin/electron app -d --inspect
+#TLINK_DEV=1 ./node_modules/.bin/electron app -d --inspect
+
+## debug mode ##
+TLINK_CONFIG_DIRECTORY=/tmp/tlink-clean \
+TLINK_PLUGINS= \
+ELECTRON_ENABLE_LOGGING=1 \
+ELECTRON_ENABLE_STACK_DUMPING=1 \
+yarn start
+
+
